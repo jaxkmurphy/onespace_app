a0204f2963f7e8df520a3821a1acc95e2c1270c8
john murphy <jaxkmurphy@gmail.com>
2025-06-06 20:17:45 +0100

    Fixed unused variable in quiz.dart

--- lib/models/quiz.dart
+++ lib/models/quiz.dart
@@ -14,19 +14,15 @@ class Quiz {
   });
 
   factory Quiz.fromMap(String id, Map<String, dynamic> map) {
-    var questionsFromMap = (map['questions'] as List<dynamic>)
+  return Quiz(
+    id: id,
+    title: map['title'] as String,
+    createdBy: map['createdBy'] as String,
+    questions: (map['questions'] as List<dynamic>? ?? [])
         .map((q) => Question.fromMap(q as Map<String, dynamic>))
-        .toList();
-
-    return Quiz(
-      id: id,
-      title: map['title'] as String,
-      createdBy: map['createdBy'] as String,
-      questions: (map['questions'] as List<dynamic>? ?? [])
-      .map((q) => Question.fromMap(q as Map<String, dynamic>))
-      .toList(),
-    );
-  }
+        .toList(),
+  );
+}
 
   Map<String, dynamic> toMap() {
     return {
