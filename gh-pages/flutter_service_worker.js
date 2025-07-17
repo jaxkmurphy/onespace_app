168fce0d07484fa48110b8d4c4db8b0f1943b4d8
john murphy <jaxkmurphy@gmail.com>
2025-07-17 21:31:31 +0100

    Deploy Flutter web build to GitHub Pages

--- gh-pages/flutter_service_worker.js
+++ gh-pages/flutter_service_worker.js
@@ -27,7 +27,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "c4b848c60f5d237c8fc857b285bf10ab
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "94ded1f59f250010bfc089307500a34d",
+"flutter_bootstrap.js": "15b259668aaf4ffe869acb40764a5568",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
